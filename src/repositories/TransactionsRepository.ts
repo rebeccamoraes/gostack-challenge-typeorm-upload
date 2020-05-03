2ec637fa0a6b9c3a8c79f7dfae942e781f2100a7
Rebecca Moraes <rebecca@ufg.br>
2020-05-03 14:28:26 -0300

    Implementando cálculo do balanço.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,17 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const balance: Balance = { income: 0, outcome: 0, total: 0 };
+
+    const findTransactions = await this.find();
+
+    findTransactions.forEach(transaction => {
+      balance[transaction.type] += transaction.value;
+    });
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 }
 
