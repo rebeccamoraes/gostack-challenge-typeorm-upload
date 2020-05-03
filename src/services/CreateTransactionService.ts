1906ab1b4ca89a5eb6f002c26478e3aac0050b6b
Rebecca Moraes <rebecca@ufg.br>
2020-05-03 15:35:05 -0300

    Implementando criação de transaction.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,57 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('Invalid transaction type.');
+    }
+    const categoryRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const findCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    const transaction = transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    if (findCategory) {
+      transaction.category_id = findCategory.id;
+    } else {
+      const newCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(newCategory);
+
+      transaction.category_id = newCategory.id;
+    }
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
